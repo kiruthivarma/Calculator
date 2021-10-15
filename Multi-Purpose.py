101d356a3eadff00f448b15ac6d4c1fa8269dac7
KiruthiVarma <88651156+KiruthiVarma@users.noreply.github.com>
2021-10-15 19:47:03 +0530

    Create Multi-Purpose.py

---
+++ Multi-Purpose.py
@@ -0,0 +1,10 @@
+print("\t\t\t\t <<- Muti-Purpose Calculator ->>")
+
+a=int(input("\n First Value  : "))
+b=int(input("\n Second Value : "))
+
+print("<<<------------------------RESULT--------------------->>> \n")
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"X",b,"=",a*b)
+print(a,"/",b,"=",a/b)
